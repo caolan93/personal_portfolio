ad915ac622884956b0fc4c4da36ba80036741046
caolan <caolan@digigrow.ie>
2023-01-06 20:37:32 +0000

    navbar container wrapper to fix sizing on 4k screens

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -5,33 +5,35 @@ import "./style.scss";
 
 const Navbar = () => {
   const [toggle, setToggle] = useState(false);
-
+  ("");
   const handleToggle = () => {
     setToggle(!toggle);
   };
 
   return (
-    <div className="navbar-container">
-      <div className="title-container">
-        <h1>Caolan Fanning Web Dev</h1>
-        <button onClick={handleToggle}>
-          <div className={`hamburger-menu ${toggle && "open"}`}>
-            <div className="line-top" />
-            <div className="line-close-top" />
-            <div className="line-mid" />
-            <div className="line-close-btm" />
-            <div className="line-btm" />
-          </div>
-        </button>
+    <div className="navbar-container-container">
+      <div className="navbar-container">
+        <div className="title-container">
+          <h1>Caolan Fanning Web Dev</h1>
+          <button onClick={handleToggle}>
+            <div className={`hamburger-menu ${toggle && "open"}`}>
+              <div className="line-top" />
+              <div className="line-close-top" />
+              <div className="line-mid" />
+              <div className="line-close-btm" />
+              <div className="line-btm" />
+            </div>
+          </button>
+        </div>
+        <nav className={`navbar-list ${toggle && "open"}`}>
+          <ul className="menu-list">
+            <li className="menu-list-item">Home</li>
+            <li className="menu-list-item">About</li>
+            <li className="menu-list-item">Skills</li>
+            <li className="menu-list-item">Contact</li>
+          </ul>
+        </nav>
       </div>
-      <nav className={`navbar-list ${toggle && "open"}`}>
-        <ul className="menu-list">
-          <li className="menu-list-item">Home</li>
-          <li className="menu-list-item">About</li>
-          <li className="menu-list-item">Skills</li>
-          <li className="menu-list-item">Contact</li>
-        </ul>
-      </nav>
     </div>
   );
 };
