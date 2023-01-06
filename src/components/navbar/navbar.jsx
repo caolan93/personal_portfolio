71bf1e60e831543851320b4982ac04939b42e251
caolan <caolan@digigrow.ie>
2023-01-06 20:24:19 +0000

    adding mobile menu with animation

--- src/components/navbar/navbar.jsx
+++ src/components/navbar/navbar.jsx
@@ -1,15 +1,35 @@
-import React from "react";
+import React, { useState } from "react";
+
+//Style
+import "./style.scss";
 
 const Navbar = () => {
+  const [toggle, setToggle] = useState(false);
+
+  const handleToggle = () => {
+    setToggle(!toggle);
+  };
+
   return (
-    <div className="flex-1 bg-slate-600 flex-wrap">
-      <nav>
+    <div className="navbar-container">
+      <div className="title-container">
         <h1>Caolan Fanning Web Dev</h1>
-        <ul className="flex flex-1">
-          <li className="flex text-white">Home</li>
-          <li className="flex text-white">About</li>
-          <li className="flex text-white">Skills</li>
-          <li className="flex text-white">Contact</li>
+        <button onClick={handleToggle}>
+          <div className={`hamburger-menu ${toggle && "open"}`}>
+            <div className="line-top" />
+            <div className="line-close-top" />
+            <div className="line-mid" />
+            <div className="line-close-btm" />
+            <div className="line-btm" />
+          </div>
+        </button>
+      </div>
+      <nav className={`navbar-list ${toggle && "open"}`}>
+        <ul className="menu-list">
+          <li className="menu-list-item">Home</li>
+          <li className="menu-list-item">About</li>
+          <li className="menu-list-item">Skills</li>
+          <li className="menu-list-item">Contact</li>
         </ul>
       </nav>
     </div>
