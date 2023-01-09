74f0dd635a24ef0424b388ce3d9fbfd993ddb6d4
caolan <caolan@digigrow.ie>
2023-01-09 21:03:33 +0000

    footer styling complete

--- src/components/footer/footer.jsx
+++ src/components/footer/footer.jsx
@@ -1,10 +1,31 @@
 import React from "react";
 
+//Assets
+import github from "../../assets/logos/github.png";
+import linkedIn from "../../assets/logos/linkedIn.png";
+
 // Style
 import "./style.scss";
 
 const Footer = () => {
-  return <div>Footer</div>;
+  return (
+    <div className="footer-container-container">
+      <div className="footer-container">
+        <a href="https://github.com/caolan93" target="_blank">
+          <img src={github} alt="Github logo icon" />
+          Github
+        </a>
+        <a
+          href="https://www.linkedin.com/in/caolan-fanning-1a7229157/"
+          target="_blank"
+        >
+          <img src={linkedIn} alt="LinkedIn logo icon" />
+          LinkedIn
+        </a>
+        <p>All Rights Reserved &copy;</p>
+      </div>
+    </div>
+  );
 };
 
 export default Footer;
