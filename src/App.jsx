20fe47650653abfb91fb8c0a20492f91c5985947
caolan <caolan@digigrow.ie>
2023-01-09 19:42:15 +0000

    changing file names to .jsx

--- src/App.jsx
+++ src/App.jsx
@@ -3,6 +3,12 @@ import Navbar from "./components/navbar/navbar";
 
 import { Route, Routes } from "react-router-dom";
 
+import Home from "./pages/home";
+import About from "./pages/about";
+import Technologies from "./pages/technologies";
+import Projects from "./pages/projects";
+import Contact from "./pages/contact";
+
 function App() {
   return (
     <>
@@ -11,7 +17,7 @@ function App() {
         <Route path="/" element={<Home />} />
         <Route path="/About" element={<About />} />
         <Route path="/Technologies" element={<Technologies />} />
-        <Route path="/Projects" element={<Project />} />
+        <Route path="/Projects" element={<Projects />} />
         <Route path="/Contact" element={<Contact />} />
       </Routes>
       <Footer />
