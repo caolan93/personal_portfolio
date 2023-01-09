49ce43a2aae192ffa83b5ff73aefc65b126207aa
caolan <caolan@digigrow.ie>
2023-01-09 19:37:04 +0000

    adding in react router dom and skeleton pages for project

--- src/App.jsx
+++ src/App.jsx
@@ -1,10 +1,20 @@
 import { useState } from "react";
 import Navbar from "./components/navbar/navbar";
 
+import { Route, Routes } from "react-router-dom";
+
 function App() {
   return (
     <>
       <Navbar />
+      <Routes>
+        <Route path="/" element={<Home />} />
+        <Route path="/About" element={<About />} />
+        <Route path="/Technologies" element={<Technologies />} />
+        <Route path="/Projects" element={<Project />} />
+        <Route path="/Contact" element={<Contact />} />
+      </Routes>
+      <Footer />
     </>
   );
 }
