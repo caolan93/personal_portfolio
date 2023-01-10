67fd0e300b2e0d5b31eaa353f1a5ed95149f865e
caolan <caolan@digigrow.ie>
2023-01-10 11:51:37 +0000

    adding in home folder route

--- src/App.jsx
+++ src/App.jsx
@@ -6,7 +6,7 @@ import Footer from "./components/footer/footer";
 
 import { Route, Routes } from "react-router-dom";
 
-import Home from "./pages/home";
+import Home from "./pages/home/home";
 import About from "./pages/about";
 import Technologies from "./pages/technologies";
 import Projects from "./pages/projects";
