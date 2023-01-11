564fedd3e6b38c1ed37b7992268980f0c5776775
caolan <caolan@digigrow.ie>
2023-01-11 21:06:27 +0000

    removing font file and adding in loader on form send

--- src/components/contact/contact.jsx
+++ src/components/contact/contact.jsx
@@ -1,10 +1,19 @@
-import React from "react";
+import React, { useState } from "react";
+
+// Installs
+import ClipLoader from "react-spinners/ClipLoader";
 import { Formik } from "formik";
 
 // Style
 import "./style.scss";
 
 const Contact = () => {
+  let [loading, setLoading] = useState(true);
+
+  const override = {
+    margin: "0 auto",
+  };
+
   return (
     <div className="contact-form">
       <h2>Get in touch!</h2>
@@ -35,50 +44,71 @@ const Contact = () => {
           isSubmitting,
         }) => (
           <form onSubmit={handleSubmit}>
-            <input
-              type="name"
-              name="name"
-              placeholder="Name"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.name}
-            />
-            <p style={{ color: "white" }}>
-              {errors.name && touched.name && errors.name}
-            </p>
-            <input
-              type="text"
-              name="phone"
-              placeholder="Phone"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.phone}
-            />
-            {errors.phone && touched.phone && errors.phone}
-            <input
-              type="email"
-              name="email"
-              placeholder="Email"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.email}
-            />
-            <p style={{ color: "white" }}>
-              {errors.email && touched.email && errors.email}
-            </p>
-            <textarea
-              type="text"
-              name="message"
-              rows={8}
-              placeholder="Message"
-              onChange={handleChange}
-              onBlur={handleBlur}
-              value={values.message}
-            />
-            {errors.message && touched.message && errors.message}
-            <button type="submit" disabled={isSubmitting}>
-              Submit
-            </button>
+            {isSubmitting ? (
+              <div
+                style={{
+                  height: "500px",
+                  justifyContent: "center",
+                  alignItems: "center",
+                }}
+              >
+                <ClipLoader
+                  color="#fff"
+                  cssOverride={override}
+                  loading={loading}
+                  size={150}
+                  aria-label="Loading Spinner"
+                  data-testid="loader"
+                />
+              </div>
+            ) : (
+              <>
+                <input
+                  type="name"
+                  name="name"
+                  placeholder="Name"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.name}
+                />
+                <p style={{ color: "white" }}>
+                  {errors.name && touched.name && errors.name}
+                </p>
+                <input
+                  type="text"
+                  name="phone"
+                  placeholder="Phone"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.phone}
+                />
+                {errors.phone && touched.phone && errors.phone}
+                <input
+                  type="email"
+                  name="email"
+                  placeholder="Email"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.email}
+                />
+                <p style={{ color: "white" }}>
+                  {errors.email && touched.email && errors.email}
+                </p>
+                <textarea
+                  type="text"
+                  name="message"
+                  rows={8}
+                  placeholder="Message"
+                  onChange={handleChange}
+                  onBlur={handleBlur}
+                  value={values.message}
+                />
+                {errors.message && touched.message && errors.message}) : (
+                <button type="submit" disabled={isSubmitting}>
+                  Submit
+                </button>
+              </>
+            )}
           </form>
         )}
       </Formik>
