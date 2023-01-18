70defc035a881adb279be3107e27e1b694fc35a7
caolan <caolan@digigrow.ie>
2023-01-18 22:17:56 +0000

    making hero styling consistent

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -7,15 +7,21 @@ const Hero = () => {
   return (
     <div className="hero-container">
       <div className="welcome-container">
-        <h1>WELCOME TO MY PERSONAL WEBSITE PORTFOLIO</h1>
-        <p>
-          My name is Caolan Fanning. I am a Web developer with experience with
-          both Frontend and Backend development using the MERN stack. Welcome to
-          my website.
-        </p>
+        <div className="title-container">
+          <h1>WELCOME TO MY PERSONAL WEBSITE PORTFOLIO</h1>
+        </div>
+        <div className="paragraph-container">
+          <p>
+            My name is Caolan Fanning. I am a Web developer with experience with
+            both Frontend and Backend development using the MERN stack. Welcome
+            to my website.
+          </p>
+        </div>
       </div>
       <div className="image-container">
-        <div className="picture"></div>
+        <div className="picture-container">
+          <div className="picture"></div>
+        </div>
       </div>
     </div>
   );
