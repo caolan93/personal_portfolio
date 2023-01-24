9f17d696406f2c39d256014fe4f4c640c1e9da63
caolan <caolan@digigrow.ie>
2023-01-24 21:38:25 +0000

    adding in array to map through

---
+++ src/assets/projectContent.js
@@ -0,0 +1,41 @@
+// Project thumbnails
+import blauair from "../assets/projects/blauair.png";
+import imt from "../assets/projects/imt.png";
+import ciac from "../assets/projects/ciac.png";
+import cmetb from "../assets/projects/cmetb.png";
+import vidacare from "../assets/projects/vidacare.png";
+import ncto from "../assets/projects/ncto.png";
+
+export const projectContent = [
+  {
+    title: "Blauair",
+    image: blauair,
+    description:
+      "Blauair is a fully fledged E-Commerce website which I helped build as part of a two person team. This was one of the first projects that I worked on in Digigrow. In this project I was first introduced to standard practices when developing large scale Web Applications such as file structure, the importance of reusable components and CMS.",
+  },
+  {
+    title: "Insure My Taxi",
+    image: imt,
+    description:
+      "Insure My Taxi was another E-Commerce website which I helped build using the MERN stack. Again working with both client and server side. In this project I furthered my ability to work with NodeJS and also got great experience working with Redux and Redux Forms.",
+  },
+  {
+    title: "Cover In A Click",
+    image: ciac,
+    description:
+      "Cover In A Click was a similar project to Insure My Taxi as it was a sister business to Insure My Taxi. Again I advanced my knowledge and skill with technolgies like Redux, NodeJS, Express, MongoDB. I also gained more knowledge with using tools such as Google Dev Tools for frontend debugging with HTTP requests and also debugging API requests in VSCode.",
+  },
+  {
+    title: "CMETB",
+    image: cmetb,
+    description:
+      "Cover In A Click was a similar project to Insure My Taxi as it was a sister business to Insure My Taxi. Again I advanced my knowledge and skill with technolgies like Redux, NodeJS, Express, MongoDB. I also gained more knowledge with using tools such as Google Dev Tools for frontend debugging with HTTP requests and also debugging API requests in VSCode.",
+  },
+
+  {
+    title: "NCTO",
+    image: ncto,
+    description:
+      "NCTO was another Wordpress I had the pleasure working on. In this project I further developed my skills with wordpress. Being a lot more comfortable with thinking and building from a Wordpress angle.",
+  },
+];
