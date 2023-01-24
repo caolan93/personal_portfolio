70533190eb52f81ee650b75d3241409628c49745
caolan <caolan@digigrow.ie>
2023-01-24 20:05:01 +0000

    adding in svg to hero

--- src/components/hero/hero.jsx
+++ src/components/hero/hero.jsx
@@ -1,11 +1,34 @@
-import React from "react";
+import React, { useEffect, useState } from "react";
+
+// Components
+import { Wave } from "../wave/wave";
 
 // Style
 import "./style.scss";
 
 const Hero = () => {
+  const [width, setWidth] = useState(false);
+
+  useEffect(() => {
+    setWidth(true);
+  }, []);
+
   return (
     <div className="hero-container">
+      {/* <svg
+        width="1926"
+        height="475"
+        viewBox="0 0 1926 475"
+        fill="none"
+        xmlns="http://www.w3.org/2000/svg"
+      >
+        <path
+          id="Vector 2"
+          d="M1 79.9995C776 1151 1050 -346.5 1925 79.9995"
+          stroke="#BFA181"
+        />
+      </svg> */}
+
       <div className="welcome-container">
         <div className="title-container">
           <h1>WELCOME TO MY PERSONAL WEBSITE PORTFOLIO</h1>
