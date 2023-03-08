eb8af48d86cd4d81435f90a7533f832b7ddb1f99
caolan <caolan@digigrow.ie>
2023-03-08 15:56:22 +0000

    adding new wave svg

--- src/components/wave/wave.jsx
+++ src/components/wave/wave.jsx
@@ -1,13 +1,34 @@
+import { motion } from "framer-motion";
+
 export const Wave = ({ width }) => {
+  const icon = {
+    hidden: {
+      pathLength: 0,
+      opacity: 0,
+    },
+    visible: {
+      pathLength: 1,
+      opacity: 1,
+    },
+  };
   return (
-    <svg
-      pathOffset={1}
-      className={`wave-svg ${width && "active"}`}
+    <motion.svg
       viewBox="0 0 1926 475"
       fill="none"
       xmlns="http://www.w3.org/2000/svg"
     >
-      <path d="M1 79.9995C776 1151 1050 -346.5 1925 79.9995" stroke="#BFA181" />
-    </svg>
+      <motion.path
+        initial="hidden"
+        animate="visible"
+        variants={icon}
+        d="M1 79.9995C776 1151 1050 -346.5 1925 79.9995"
+        stroke="#BFA181"
+        strokeWidth={3}
+        transition={{
+          default: { duration: 1, ease: "easeInOut" },
+          fill: { duration: 1, ease: [1, 0, 0.8, 1] },
+        }}
+      />
+    </motion.svg>
   );
 };
