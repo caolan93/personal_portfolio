b1db176149ce5f57f1cee0e91004363dc64abc7b
caolan <caolan@digigrow.ie>
2023-03-08 15:56:49 +0000

    adding about component to homepage

--- src/pages/home/home.jsx
+++ src/pages/home/home.jsx
@@ -7,12 +7,14 @@ import Contact from "../../components/contact/contact";
 import Hero from "../../components/hero/hero";
 import Technologies from "../../components/technologies/technologies";
 import Projects from "../../components/projects/projects";
+import About from "../../components/about/about";
 
 const Home = () => {
   return (
     <div className="home-container-container">
       <div className="home-container">
         <Hero />
+        <About />
         <Technologies />
         <Projects />
         <Contact />
