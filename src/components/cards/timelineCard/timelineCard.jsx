edd0d9846eec7e302feabd681cd5f0e02a2c534e
caolan <caolan@digigrow.ie>
2023-03-08 15:56:39 +0000

    adding boiler plate for timeline card

---
+++ src/components/cards/timelineCard/timelineCard.jsx
@@ -0,0 +1,42 @@
+import React from "react";
+
+//Style
+import "./style.scss";
+
+const TimelineCard = () => {
+  return (
+    <div className="timeline-card-container">
+      <div className="timeline-card">
+        <div className="title">Timeline Title</div>
+        <ul>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem,
+            veritatis.
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem,
+            veritatis.
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem,
+            veritatis.
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem,
+            veritatis.
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem,
+            veritatis.
+          </li>
+          <li>
+            Lorem ipsum dolor sit amet consectetur, adipisicing elit. Autem,
+            veritatis.
+          </li>
+        </ul>
+      </div>
+    </div>
+  );
+};
+
+export default TimelineCard;
